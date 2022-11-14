1f9f19e282d6d6be9424ddbbf8e98159f5365451
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-11-14 14:14:57 -0300

    :sparkles: feat: added StatelessWidget to app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const myApp(),
+    /* Prefer const with constant constructors. */
+  );
+}
+
+// ignore: camel_case_types
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+  /* key to constructors */
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
